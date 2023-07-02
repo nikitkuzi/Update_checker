a6dd6a24db298acf6de4b456dd565314fcb74180
nikitkuzi <nikitkuzi@gmail.com>
2023-07-02 18:06:30 +0300

    get_path

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
 import json
-def loadUserBookmarks():
-    """Load user bookmarks"""
-    p = os.path.normpath(r"Google\Chrome\User Data\Default\Bookmarks")
-    path = os.getenv('LOCALAPPDATA')
 
-    with open(os.path.join(path, p), encoding='utf-8') as file:
-       print(json.load(file))
+
+def load_user_bookmarks():
+    """Load user bookmarks"""
+
+    with open(get_path("Bookmarks"), encoding='utf-8') as file:
+        data = json.load(file)
+
+    folders = {'manga', 'manga1', 'manga2', 'manga3'}
+    urls = []
+    for folder in data['roots']['bookmark_bar']['children']:
+        if folder['name'] in folders:
+            for bookmark in folder['children']:
+                urls.append(bookmark['url'])
+    print(len(urls))
+    print(urls)
+
+
+def get_path(filename: str):
+    return os.path.join(os.getenv('LOCALAPPDATA'), r"Google\Chrome\User Data\Default", filename)
+
 
 if __name__ == '__main__':
-    loadUserBookmarks()
+    load_user_bookmarks()
 
 
