c10669c558499b847131628f971182d11dda3767
nikitkuzi <nikitkuzi@gmail.com>
2023-07-03 22:23:04 +0300

    browser history module

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import os
 import json
+import sqlite3
+from typing import Callable
+from browser_history.browsers import Chrome
 
 
-def load_user_bookmarks():
+def load_user_bookmarks(folders: set[str]):
     """Load user bookmarks"""
-
-    with open(get_path("Bookmarks"), encoding='utf-8') as file:
+    with open(get_path('Bookmarks'), encoding='utf-8') as file:
         data = json.load(file)
 
-    folders = {'manga', 'manga1', 'manga2', 'manga3'}
     urls = []
+
     for folder in data['roots']['bookmark_bar']['children']:
         if folder['name'] in folders:
             for bookmark in folder['children']:
                 urls.append(bookmark['url'])
-    print(len(urls))
+
+
+def get_path(filename: str) -> str:
+    """Returns path to filename from chrome user default folder"""
+    return os.path.join(os.getenv('LOCALAPPDATA'), r'Google\Chrome\User Data\Default', filename)
+
+
+def get_history(browser: Callable):
+    outputs = browser().fetch_history(desc=True)
+    history = [hstr[1] for hstr in outputs.histories]
+
+    print(history[:20])
+
+
+def get_bookmarks(browser: Callable, folders: set[str]):
+    outputs = browser().fetch_bookmarks()
+    bookmarks = outputs.bookmarks
+
+    urls = []
+    for bookmark in bookmarks:
+        if any([bookmark[3].endswith(folder) for folder in folders]):
+            urls.append(bookmark[1])
     print(urls)
 
 
-def get_path(filename: str):
-    return os.path.join(os.getenv('LOCALAPPDATA'), r"Google\Chrome\User Data\Default", filename)
+def load_visited_pages():
+    pass
 
 
 if __name__ == '__main__':
-    load_user_bookmarks()
-
-
+    folders = {'manga', 'manga1', 'manga2', 'manga3'}
+    load_user_bookmarks(folders)
+    get_history(Chrome)
+    get_bookmarks(folders)
