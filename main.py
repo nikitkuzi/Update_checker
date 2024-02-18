d840af731bd1c61de09321ca8efe4f024b73bf3e
nikitkuzi <nikitkuzi@gmail.com>
2024-02-18 15:50:33 +0300

    Added timeit decorator

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 import json
 import sqlite3
+import time
 from typing import Callable
 from browser_history.browsers import Chrome
 import platform
+from browsers import Chrome
 
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        curr_time = time.time()
+        res = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time() - curr_time}ms to run")
+        return res
+
+    return wrapper
+
+@time_it
 def load_user_bookmarks(folders: set[str]):
     """Load user bookmarks"""
     try:
@@ -19,14 +32,14 @@ def load_user_bookmarks(folders: set[str]):
         if folder['name'] in folders:
             for bookmark in folder['children']:
                 bookmarks_urls.append(bookmark['url'])
-
+    print(bookmarks_urls)
 
 
 def get_path(filename: str) -> str:
-    """Returns path to filename from chrome user default folder"""
+    """Returns path to filename from chrome user default folder."""
     current_system = platform.system()
     if current_system == "Linux":
-        return os.path.join(os.path.expanduser('~'),".config/google-chrome/Default", filename)
+        return os.path.join(os.path.expanduser('~'), ".config/google-chrome/Default", filename)
     elif current_system == "Windows":
         return os.path.join(os.getenv('LOCALAPPDATA'), r'Google\Chrome\User Data\Default', filename)
     else:
@@ -37,9 +50,10 @@ def get_history(browser: Callable):
     outputs = browser().fetch_history(desc=True)
     history = [hstr[1] for hstr in outputs.histories]
 
-    print(history[:20])
+    print(len(history), history[:20])
 
 
+@time_it
 def get_bookmarks(browser: Callable, folders: set[str]):
     outputs = browser().fetch_bookmarks()
     bookmarks = outputs.bookmarks
@@ -57,6 +71,8 @@ def load_visited_pages():
 
 if __name__ == '__main__':
     folders = {'manga', 'manga1', 'manga2', 'manga3', 'manga4'}
-    load_user_bookmarks(folders)
+    # load_user_bookmarks(folders)
     # get_history(Chrome)
-    # get_bookmarks(folders)
+    # get_bookmarks(Chrome, folders)
+    # a = Chrome()
+    b = Chrome()
