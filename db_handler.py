0996e83f4e63a51c61ec18d89b7ce753d095eba6
nikitkuzi <nikitkuzi@gmail.com>
2024-02-20 13:40:36 +0300

    refactor

--- db_handler.py
+++ db_handler.py
@@ -1,36 +1,56 @@
+import abc
 import sqlite3
 import os
 
 
-class DbHandler:
+class DbHandler(abc.ABC):
+
+    @abc.abstractmethod
+    def update(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]) -> None:
+        pass
+
+    @abc.abstractmethod
+    def create(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]) -> None:
+        pass
+
+    @abc.abstractmethod
+    def reset(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def _execute(self, sql: str,
+                 values: [tuple[tuple[str, str]] | list[tuple[str, str]] | None] = None) \
+            -> [list[str] | None]:
+        pass
+
+
+class Bookmarked(DbHandler):
     __last_chapters_bookmarked = "last_chapters_bookmarked"
-    __last_visited = "last_visited"
 
     def __init__(self):
         self.__create_dbs()
 
-    def update_last_chapters_bookmarked(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]):
+    def update(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]):
         """Updates db of last chapters from bookmarked urls.
         values: tuple(chapter,url)"""
         sql = "update last_chapters_bookmarked set chapter = ? where url = ?"
-        self.__execute_last_chapters_bookmarked(sql, values)
+        self._execute(sql, values)
 
-    def create_last_chapters_bookmarked(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]):
+    def create(self, values: [tuple[tuple[str, str]] | list[tuple[str, str]]]):
         sql = "Insert or ignore into last_chapters_bookmarked(chapter, url) values(?,?)"
-        self.__execute_last_chapters_bookmarked(sql, values)
+        self._execute(sql, values)
 
-    def get_last_chapters_bookmarked(self):
+    def get_last_chapters(self):
         sql = "select * from last_chapters_bookmarked"
-        return self.__execute_last_chapters_bookmarked(sql)
+        return self._execute(sql)
 
-    def reset_last_chapters_bookmarked(self):
+    def reset(self):
         os.remove(self.__last_chapters_bookmarked)
         self.__create_dbs()
 
-    def __execute_last_chapters_bookmarked(self, sql: str,
-                                           values: [tuple[tuple[str, str]] | list[tuple[str, str]] | None] = None) -> \
-            [
-                list[str] | None]:
+    def _execute(self, sql: str,
+                 values: [tuple[tuple[str, str]] | list[tuple[str, str]] | None] = None) \
+            -> [list[str] | None]:
         with sqlite3.connect(self.__last_chapters_bookmarked) as conn:
             cur = conn.cursor()
             if values:
@@ -46,11 +66,6 @@ class DbHandler:
         if not os.path.exists(self.__last_chapters_bookmarked):
             sql_create = "CREATE TABLE last_chapters_bookmarked(url text primary key, chapter text);"
             with sqlite3.connect(self.__last_chapters_bookmarked) as conn:
-                conn.executescript(sql_create)
-                conn.commit()
-        if not os.path.exists(self.__last_visited):
-            sql_create = "CREATE TABLE last_visited(url text primary key, date_visited date)"
-            with sqlite3.connect(self.__last_visited) as conn:
                 conn.executescript(sql_create)
                 conn.commit()
 
