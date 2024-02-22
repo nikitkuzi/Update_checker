ccd4515d98de02333e67daebafe9dcadb83eddbf
nikitkuzi <nikitkuzi@gmail.com>
2024-02-22 11:37:07 +0300

    time it decorator

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 import platform
-
+import time
 
 class SupportedWebsite:
     __supported = {"reaperscans.com"}
     @classmethod
     def supported_website(cls, url: str) -> bool:
         return url in cls.__supported
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        curr_time = time.time()
+        res = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time() - curr_time}ms to run")
+        return res
+
+    return wrapper
